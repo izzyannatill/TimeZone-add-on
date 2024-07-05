d0050b3a866aac7f7c7bbb81cf2716144955bfd8
izzyannatill <izzyannatill@gmail.com>
2024-07-05 10:24:26 +0100

    Added a function to alert for cities when selected from dropdown

---
+++ src/script.js
@@ -0,0 +1,18 @@
+function showSelectedCity(event) {
+if (event.target.value.length > 0) {
+console.log(event.target.value);
+}
+if (event.target.value == "paris") {
+alert(Paris);
+if (event.target.value == "sydney") {
+alert("Sydney"); 
+if (event.target.value == "tokyo") {
+    alert("Tokyo");
+}
+}
+}
+}
+
+let citySelect = document.querySelector ("#cities");
+
+citySelect.addEventListener("change", showSelectedCity);
