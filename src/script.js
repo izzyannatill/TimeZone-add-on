48fba29237a31a7e19a3e80b4517c9f738357a5c
izzyannatill <izzyannatill@gmail.com>
2024-07-05 10:38:50 +0100

    Found the error breaking the code
    I had two "" "" in the HTML for the selects name and id so the JS wasn't pulling correctly

--- src/script.js
+++ src/script.js
@@ -2,17 +2,17 @@ function showSelectedCity(event) {
 if (event.target.length > 0) {
     console.log(event.target.value);
 }
-if (event.target.value == "paris") {
+if (event.target.value === "paris") {
     alert("Paris!");
 }
-if (event.target.value == "sydney") {
+if (event.target.value === "sydney") {
     alert("Sydney!");
 }
-if (event.target.value == "tokyo") {
+if (event.target.value === "tokyo") {
     alert("Tokyo!");
 }
 }
 
-let citySelect = document.querySelector ("#cities");
+let citySelect = document.querySelector("#cities");
 
 citySelect.addEventListener("change", showSelectedCity);
